a3422833452da3cc9d9b0929dfa93d4ecb87213d
AlvDaDalv <161541819+AlvDaDalv@users.noreply.github.com>
2024-12-30 09:46:46 -0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -83,7 +83,7 @@ def take_photo():
 
         time.sleep(2.0)
         name = img_gen("AlvinL")
-        #picam2.switch_mode_and_capture_file(capture_config, f'.{name}')
+        picam2.switch_mode_and_capture_file(capture_config, f'.{name}')
         
         '''
         if accelx > THRESHOLD or accely > THRESHOLD or accelz > THRESHOLD:
