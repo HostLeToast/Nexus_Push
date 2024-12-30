18877984f679c401a52c1ca6d045b9911a07b7d4
AlvDaDalv <161541819+AlvDaDalv@users.noreply.github.com>
2024-12-30 12:56:02 -0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,15 +1,3 @@
-"""
-The Python code you will write for this module should read
-acceleration data from the IMU. When a reading comes in that surpasses
-an acceleration threshold (indicating a shake), your Pi should pause,
-trigger the camera to take a picture, then save the image with a
-descriptive filename. You may use GitHub to upload your images automatically,
-but for this activity it is not required.
-
-The provided functions are only for reference, you do not need to use them. 
-You will need to complete the take_photo() function and configure the VARIABLES section
-"""
-
 #AUTHOR: Alvin Lai
 #DATE: 12/12/24
 
@@ -33,7 +21,6 @@ accel_gyro = LSM6DS(i2c)
 mag = LIS3MDL(i2c)
 picam2 = Picamera2()
 
-
 def git_push():
     """
     This function is complete. Stages, commits, and pushes new images to your GitHub repo.
@@ -52,7 +39,6 @@ def git_push():
     except:
         print('Couldn\'t upload to git')
 
-
 def img_gen(name):
     """
     This function is complete. Generates a new image name.
@@ -64,49 +50,41 @@ def img_gen(name):
     imgname = (f'{REPO_PATH}/{FOLDER_PATH}/{name}{t}.jpg')
     return imgname
 
-
 def take_photo():
     """
     This function is NOT complete. Takes a photo when the FlatSat is shaken.
-    Replace psuedocode with your own code.
+    Replace pseudocode with your own code.
     """
     while True:
         accelx, accely, accelz = accel_gyro.acceleration
 
-        #CHECKS IF READINGS ARE ABOVE THRESHOLD
-            #PAUSE
-            #name = ""     #First Name, Last Initial  ex. MasonM
-            #TAKE PHOTO
-            #PUSH PHOTO TO GITHUB
-        
-        #PAUSE
-
-        time.sleep(2.0)
-        name = img_gen("AlvinL")
-        picam2.switch_mode_and_capture_file(capture_config, f'.{name}')
-        
-        '''
+        # CHECKS IF READINGS ARE ABOVE THRESHOLD
         if accelx > THRESHOLD or accely > THRESHOLD or accelz > THRESHOLD:
-            picam2.start()
+            # PAUSE for 2 seconds to stabilize camera
             time.sleep(2.0)
+            
             name = img_gen("AlvinL")
+            
+            # Capture photo using autofocus and save with the generated name
             picam2.switch_mode_and_capture_file(capture_config, f'.{name}')
+            
+            # Push the photo to GitHub
             git_push()
 
-        time.sleep(5.0)
-        '''
-
-
-
-
+        time.sleep(2.0)  # Adjust sleep time as needed for your application
 
 def main():
     global capture_config
     capture_config = picam2.create_still_configuration(main={"size": (5120, 2880)})
-    picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous, "LensPosition": 300.0})
+    
+    # Use autofocus and disable manual focus
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Auto})  # Enable autofocus
+    
+    # Start the camera
     picam2.start(show_preview=False)
+    
+    # Start taking photos based on shake detection
     take_photo()
-
 
 if __name__ == '__main__':
     main()
