514b81c0e5df8cccb423b5dadf2b3b9a3c394ce6
AlvDaDalv <161541819+AlvDaDalv@users.noreply.github.com>
2025-01-02 08:54:25 -0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -84,6 +84,8 @@ def take_photo():
         #time.sleep(2.0)
         name = img_gen("AlvinL")
         picam2.capture_file(f'.{name}')
+
+        print("why isn't it taking a pic")
         
         '''
         if accelx > THRESHOLD or accely > THRESHOLD or accelz > THRESHOLD:
@@ -103,6 +105,7 @@ def main():
     picam2.configure(picam2.create_still_configuration(main={"size": (1920, 1080)}))
     picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous, "LensPosition": 0.0})
     picam2.start(show_preview=False)
+    print("code is loaded")
     take_photo()
 
 
