bc0877147d7258a8dfd0bcff49204d6151b4522c
AlvDaDalv <161541819+AlvDaDalv@users.noreply.github.com>
2025-01-02 10:32:39 -0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -102,10 +102,10 @@ def take_photo():
 
 def main():
     global capture_config
-    capture_config = picam2.create_continuous_configuration(main={"size": (4056, 3040)})
-    #picam2.configure(picam2.create_still_configuration(main={"size": (1920, 1080)}))
+    #capture_config = picam2.create_continuous_configuration(main={"size": (4056, 3040)})
+    picam2.configure(picam2.create_still_configuration(main={"size": (1920, 1080)}))
     picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous, "LensPosition": 0.0})
-    picam2.configure(capture_config)
+    #picam2.configure(capture_config)
     picam2.zoom = (0, 0, 1, 1)
     picam2.start()
     take_photo()
